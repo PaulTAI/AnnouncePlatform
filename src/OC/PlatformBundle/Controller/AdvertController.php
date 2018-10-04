b156d34c238dc3821f993e5cd09187231bba3c8a
Paul TAITINGER <paul.taitinger@open-groupe.com>
2018-10-04 14:15:00 +0200

    modif14h14

--- src/OC/PlatformBundle/Controller/AdvertController.php
+++ src/OC/PlatformBundle/Controller/AdvertController.php
@@ -5,6 +5,7 @@
 namespace OC\PlatformBundle\Controller;
 
 use OC\PlatformBundle\Entity\Advert;
+use OC\PlatformBundle\Entity\Image;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
@@ -99,6 +100,14 @@ class AdvertController extends Controller
     // On peut ne pas définir ni la date ni la publication,
     // car ces attributs sont définis automatiquement dans le constructeur
 
+    //Création de l'entité Image
+    $image = new Image();
+    $image->setUrl('http://sdz-upload.s3.amazonaws.com/prod/upload/job-de-reve.jpg');
+    $image->setAlt('Job de rêve');
+
+    // On lie l'image à l'annonce
+    $advert->setImage($image);
+
     // On récupère l'EntityManager
     $em = $this->getDoctrine()->getManager();
 
@@ -114,16 +123,7 @@ class AdvertController extends Controller
 
       // Puis on redirige vers la page de visualisation de cettte annonce
       return $this->redirectToRoute('oc_platform_view', array('id' => $advert->getId()));
-    }
-    /* // On récupère le service
-    $antispam = $this->container->get('oc_platform.antispam');
-
-    // Je pars du principe que $text contient le texte d'un message quelconque
-    $text = '...';
-    if ($antispam->isSpam($text)) {
-      throw new \Exception('Votre message a été détecté comme spam !');
-    } */
-    
+    }    
     // Ici le message n'est pas un spam
     // Si on n'est pas en POST, alors on affiche le formulaire
     return $this->render('OCPlatformBundle:Advert:add.html.twig', array('advert' => $advert));
